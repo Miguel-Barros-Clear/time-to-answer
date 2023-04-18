1096df0dc1d83ad77c9ddf7ef2c62a33dd07dcf8
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-18 10:38:54 -0300

    feat: protecting private routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
   
   get 'inicio', to: 'site/welcome#index'
