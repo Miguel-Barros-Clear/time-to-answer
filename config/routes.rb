8273d21cfe362c129cdfb715d878ca8ab34c94dd
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-19 12:08:11 -0300

    feat: create a new administrator into admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-    resources :admins, only: [:index, :edit, :update]
-  end
   namespace :site do
-    get 'welcome/index'
-  end
-  namespace :admins_backoffice do
     get 'welcome/index'
   end
   namespace :users_backoffice do
     get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+    resources :admins, except: [:delete]  
   end
 
   devise_for :admins
