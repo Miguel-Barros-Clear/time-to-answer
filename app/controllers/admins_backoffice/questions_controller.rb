5293e2e4f357b92b28048e11464323bee661f353
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-20 10:46:59 -0300

    feat: creating controller for questions views

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,10 +1,8 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
-
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.all.order(:subject).page(params[:page])
   end
-
   def new
     @question = Question.new
   end
@@ -12,27 +10,26 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   def create
     @question = Question.new(params_question)
     if @question.save
-      redirect_to admins_backoffice_subjects_path, notice: "Questão criado com sucesso"
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
     else
       render :new
     end
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def update
     if @question.update(params_question)
-      redirect_to admins_backoffice_subjects_path, notice: "Questão atualizado com sucesso"
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
     else
       render :edit
     end
   end
 
   def destroy
-     if @question.destroy
-      redirect_to admins_backoffice_subjects_path, notice: "Questão excluído com sucesso"
+    if @question.destroy
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
     else
       render :index
     end
@@ -41,10 +38,10 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   private
 
   def params_question
-    params.require(:question).permit(:description)
+    params.require(:question).permit(:description, :subject_id)
   end
 
   def set_question
     @question = Question.find(params[:id])
-  end  
+  end
 end
